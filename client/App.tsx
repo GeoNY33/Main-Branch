01b8561aee00677debfa86f0ec2d0320188ade0e
Hank Jackson <hjackson277@gmail.com>
2022-07-11 17:48:56 -0400

    added movies.json and listing on landing page

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,41 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './stylesheets/styles.css';
 
+
 const App = () => {
+
+  const [allMovies, setMovies] = useState([]);
+
+  useEffect(() => {
+
+    const getMovies = async () => {
+      const movies: any = [];
+
+      const res = await fetch('../movies.json')
+      const data = await res.json();
+
+      data.forEach((movie: any) => {
+        movies.push(
+        <div>
+          <h3>{movie.name}</h3>
+          <img src={movie.image}/>
+          <br/>
+          <br/>
+        </div>
+        )
+      })
+
+      setMovies(movies)
+    }
+
+    getMovies();
+  }, []);
+
   return (
     <div>
-      <h1>Testing yet again</h1>
+      <h1>Movie ELO</h1>
+      {allMovies}
     </div>
   );
 }
