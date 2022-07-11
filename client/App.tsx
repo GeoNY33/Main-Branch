06a58e12025d5c051c109453f0d75a33559054f8
Hank Jackson <hjackson277@gmail.com>
2022-07-11 18:51:31 -0400

    added extra data for movies

--- client/App.tsx
+++ client/App.tsx
@@ -11,20 +11,23 @@ const App = () => {
 
     const getMovies = async () => {
       const movies: any = [];
-      const res = await fetch('../movies.json')
+      const res = await fetch('../movies.json');
       const data = await res.json();
 
       data.forEach((movie: any) => {
+  
         movies.push(
           <div>
             <h3>{movie.name}</h3>
+            <div>{movie.description}</div>
+            <div>{movie.genre[0]}</div>
             <img src={movie.image}/>
             <br/>
             <br/>
           </div>
         )
       })
-      setMovies(movies)
+      setMovies(movies);
     }
 
     getMovies();
