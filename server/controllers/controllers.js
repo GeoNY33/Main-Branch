52b4943f6d453faa91633e202342fe516fc1e4cf
Hank Jackson <hjackson277@gmail.com>
2022-07-12 15:11:03 -0400

    preparing for merge

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -11,7 +11,6 @@ const controllers = {
   insertAllMovies: (req, res, next) => {
     const moviePath = path.join(__dirname, '../../movies.json')
     const movies = JSON.parse(fs.readFileSync(moviePath, 'utf-8'));
-
     for (let i = 0; i < movies.length; i++){
       const query = `INSERT INTO movie_app.movies (title, image, genre) VALUES ('${movies[i].name}', '${movies[i].image}', '${movies[i].genre[0]}')`;
       db.query(query)
@@ -19,23 +18,23 @@ const controllers = {
           console.log(data);
         })
     }
-
     res.locals.movies = movies;
     return next();
   },
   
+
   newUser: (req, res, next) => {
-    console.log(req.body)
     bcrypt.hash(req.body.password, 10, (err, hash) => {
-        const query = `INSERT INTO movie_app.users (username, password) VALUES ('${req.body.username}', '${hash}') RETURNING *`;
-        db.query(query)
-          .then(data => {
-            res.locals.userinfo = data.rows[0]
-            return next();
-          })
-      })
+      const query = `INSERT INTO movie_app.users (username, password) VALUES ('${req.body.username}', '${hash}') RETURNING *`;
+      db.query(query)
+        .then(data => {
+          res.locals.userinfo = data.rows[0]
+          return next();
+        })
+    })
   },
   
+
   getRandomMovie: (req, res, next) => {
     const values = [Math.floor(Math.random() * 182)];
     const query = 'SELECT * FROM movie_app.movies WHERE _id = $1';
