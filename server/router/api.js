862b58fc6de32be07630d8ebfac2758d0d4960f0
Hank Jackson <hjackson277@gmail.com>
2022-07-12 13:05:55 -0400

    added controllers and updated db with movies

--- server/router/api.js
+++ server/router/api.js
@@ -1,5 +1,31 @@
 const express = require('express');
+const controllers = require('../controllers/controllers');
 
 const router = express.Router();
+
+//do not use lol
+router.get('/insertAllMovies', controllers.insertAllMovies, (req, res) => {
+  res.status(200).json(res.locals.movies);
+})
+
+//get random movie
+router.get('/getRandomMovie', controllers.getRandomMovie, (req, res) => {
+  res.status(200).json(res.locals.movie);
+})
+
+//submit change in ELO on selection
+
+//fetch all movies to display ranking
+
+//create new user
+router.post('/newUser', controllers.newUser, (req, res) => {
+  res.status(200).json(res.locals.userinfo)
+})
+
+//verify existing user
+  //start session
+
+
+
 
 module.exports = router;
